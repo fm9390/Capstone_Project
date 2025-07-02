085afc58bfee690267f26534d804b79b711927bc
Fanny Mayer <fannymayer@MacBook-Air.local>
2025-07-02 12:04:06 +0200

    Add EC2 instance 'MyFirstInstance'

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_instance" "my_first_instance" {
+  ami           = "ami-040361ed8686a66a2"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyFirstInstance"
+  }
+}
