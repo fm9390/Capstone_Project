1a4f2c6e7819d355c8b600f5776a527dced82c40
Fanny Mayer <fannymayer@MacBook-Air.local>
2025-07-02 12:07:05 +0200

    Create Subnet 'Public Subnet 1'

--- main.tf
+++ main.tf
@@ -27,3 +27,11 @@ resource "aws_vpc" "my_first_VPC" {
     Name = "MyFirstVPC"
   }
 }
+
+resource "aws_subnet" "my_first_subnet" {
+  vpc_id     = aws_vpc.my_first_VPC.id
+  cidr_block = "10.0.0.0/24"
+  tags       = {
+   Name = "Public Subnet 1"
+   }
+}
