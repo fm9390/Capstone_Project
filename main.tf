5f83ac025a9e0a25cd210093010c6eff54f8e5fb
Fanny Mayer <fannymayer@MacBook-Air.local>
2025-07-03 17:09:53 +0200

    Restructuring code, adding security groups

--- main.tf
+++ main.tf
@@ -11,14 +11,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "my_first_instance" {
-  ami           = "ami-040361ed8686a66a2"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "MyFirstInstance"
-  }
-}
-
 resource "aws_vpc" "my_first_VPC" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -32,15 +24,37 @@ resource "aws_subnet" "my_public_subnet1" {
   vpc_id     = aws_vpc.my_first_VPC.id
   cidr_block = "10.0.1.0/24"
   tags       = {
-   Name = "PublicSubnet1"
+   Name = "MyPublicSubnet1"
    }
+}
+
+resource "aws_instance" "my_public_instance1" {
+  ami           = "ami-040361ed8686a66a2"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.my_public_subnet1.id
+  key_name = "vockey"
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  tags = {
+    Name = "MyPublicInstance1"
+  }
+}
+
+resource "aws_eip" "bastion_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "BastionEIP"
+  }
+}
+resource "aws_eip_association" "bastion_eip_assoc" {
+  instance_id   = aws_instance.my_public_instance1.id
+  allocation_id = aws_eip.bastion_eip.id
 }
 
 resource "aws_internet_gateway" "my_first_IGW" {
   vpc_id = aws_vpc.my_first_VPC.id
 
   tags = {
-    Name = "IGW"
+    Name = "MyFirstIGW"
   }
 }
 
@@ -60,7 +74,75 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.my_first_routetable.id
   }
 
-resource "aws_eip" "my_first_eib" {
-  instance = aws_instance.my_first_instance.id
-  domain   = "vpc"
+resource "aws_subnet" "my_private_subnet1" {
+  vpc_id     = aws_vpc.my_first_VPC.id
+  cidr_block = "10.0.2.0/24"
+  tags       = {
+   Name = "MyPrivateSubnet1"
+   }
+}
+
+resource "aws_instance" "my_private_instance1" {
+  ami           = "ami-040361ed8686a66a2"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.my_private_subnet1.id
+  key_name = "vockey"
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  tags = {
+    Name = "MyPrivateInstance1"
+  }
+}
+
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Allow SSH from my IP, HTTP/HTTPS from anywhere"
+  vpc_id      = aws_vpc.my_first_VPC.id
+  # SSH (nur von deiner IP)
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["89.46.11.152/32"] # my IP
+  }
+  # HTTP (für WordPress)
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion_sg"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  description = "Allow MySQL access from Bastion Host"
+  vpc_id      = aws_vpc.my_first_VPC.id
+  ingress {
+    description = "Allow MySQL from Bastion Host"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id] # Bastion Host IP
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "db_sg"
+  }
 }
