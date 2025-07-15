b71ad30618d5551d853ed5df025872d0e3243405
Fanny Mayer <fannymayer@MacBook-Air.local>
2025-07-15 20:25:30 +0200

    Attach ALB ARN to launch template for web servers

--- main.tf
+++ main.tf
@@ -119,6 +119,7 @@ name                      = "web-asg"
   ]
   health_check_type         = "EC2"
   health_check_grace_period = 300
+  target_group_arns = [aws_lb_target_group.web_lbtg.arn]
   launch_template {
     id      = aws_launch_template.web_lt.id
     version = "$Latest"
